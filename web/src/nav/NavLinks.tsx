9e96b7ca92270fb1c8be79a6041ac688a4a15619
Quinn Slack <qslack@qslack.com>
2019-02-03 08:47:04 +0100

    improve

--- web/src/nav/NavLinks.tsx
+++ web/src/nav/NavLinks.tsx
@@ -32,6 +32,14 @@ export class NavLinks extends React.PureComponent<Props> {
     public render(): JSX.Element | null {
         return (
             <ul className="nav-links nav align-items-center pl-2 pr-1">
+                {/* Show "Search" link on small screens when GlobalNavbar hides the SearchNavbarItem. */}
+                {this.props.location.pathname !== '/welcome' && (
+                    <li className="nav-item d-sm-none">
+                        <Link className="nav-link" to="/search">
+                            Search
+                        </Link>
+                    </li>
+                )}
                 {showDotComMarketing && this.props.location.pathname !== '/welcome' && (
                     <li className="nav-item">
                         <Link to="/welcome" className="nav-link">
@@ -64,11 +72,13 @@ export class NavLinks extends React.PureComponent<Props> {
                 )}
                 {!this.props.authenticatedUser && (
                     <>
-                        <li className="nav-item">
-                            <Link to="/extensions" className="nav-link">
-                                Extensions
-                            </Link>
-                        </li>
+                        {this.props.location.pathname !== '/welcome' && (
+                            <li className="nav-item">
+                                <Link to="/extensions" className="nav-link">
+                                    Extensions
+                                </Link>
+                            </li>
+                        )}
                         {this.props.location.pathname !== '/sign-in' && (
                             <li className="nav-item mx-1">
                                 <Link className="nav-link btn btn-primary" to="/sign-in">
@@ -83,20 +93,24 @@ export class NavLinks extends React.PureComponent<Props> {
                                 </a>
                             </li>
                         )}
-                        <li className="nav-item">
-                            <Link to="/help" className="nav-link">
-                                Help
-                            </Link>
-                        </li>
+                        {this.props.location.pathname !== '/welcome' && (
+                            <li className="nav-item">
+                                <Link to="/help" className="nav-link">
+                                    Help
+                                </Link>
+                            </li>
+                        )}
                     </>
                 )}
-                <CommandListPopoverButton
-                    menu={ContributableMenu.CommandPalette}
-                    extensionsController={this.props.extensionsController}
-                    platformContext={this.props.platformContext}
-                    toggleVisibilityKeybinding={this.props.keybindings.commandPalette}
-                    location={this.props.location}
-                />
+                {this.props.location.pathname !== '/welcome' && (
+                    <CommandListPopoverButton
+                        menu={ContributableMenu.CommandPalette}
+                        extensionsController={this.props.extensionsController}
+                        platformContext={this.props.platformContext}
+                        toggleVisibilityKeybinding={this.props.keybindings.commandPalette}
+                        location={this.props.location}
+                    />
+                )}
                 {this.props.authenticatedUser && (
                     <li className="nav-item">
                         <UserNavItem
